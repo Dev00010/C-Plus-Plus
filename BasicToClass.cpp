fc6e29909c3053877b5caa36612f9b388872db0e
Dev <joisardev@gmail.com>
2022-01-15 14:50:30 +0530

    Basic to Class Type Conversion

---
+++ BasicToClass.cpp
@@ -0,0 +1,27 @@
+#include<iostream> 
+using namespace std; 
+class Weight 
+{ 
+    int kg; 
+    public: 
+    Weight(int x) 
+	{ 
+        kg=x/100; 
+    }  
+    Weight() 
+   { 
+       kg=0; 
+   } 
+    void WeightPrint() 
+	{ 
+      cout<<"\nKg = "<<kg; 
+    }
+    };
+main()
+{
+	Weight w1; 
+    int grams=200; 
+    cout<<"\nGrams = "<<grams; 
+    w1=grams; // basic to class 
+    w1.WeightPrint(); 
+}
