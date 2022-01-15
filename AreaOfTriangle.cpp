f95c5a4f901ebd170790b5667a61bd0bc88eef49
Dev <joisardev@gmail.com>
2022-01-15 14:36:48 +0530

    Prints area of triangle

---
+++ AreaOfTriangle.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int l,b,h;
+	cout<<endl<<"Enter length=> ";
+	cin>>l;
+	cout<<endl<<"Enter breadth=> ";
+	cin>>b;
+	cout<<endl<<"Enter heigth=> ";
+	cin>>h;
+	cout<<endl<<"Area of Triangle=> "<<l*b*h;
+}
