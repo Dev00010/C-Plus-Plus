9130de43b556b4452726dfaef2ab2874c7e01102
Dev <joisardev@gmail.com>
2022-01-15 15:09:58 +0530

    Print odd or even using if..else condition

---
+++ If_Else_Odd_Even.cpp
@@ -0,0 +1,16 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a;
+	cout<<endl<<"Enter Number 1=> ";
+	cin>>a;
+	if(a%2==0)
+	{
+		cout<<endl<<a<<" is an Even Number";
+	}
+	else
+	{
+		cout<<endl<<a<<" is an Odd Number";
+	}
+}
