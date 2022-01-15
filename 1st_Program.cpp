656d86d636b96ce924c3238bc712a2c38e62b5d7
Dev <joisardev@gmail.com>
2022-01-15 14:35:28 +0530

    My First C++ Program

---
+++ 1st_Program.cpp
@@ -0,0 +1,14 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	float a,b;
+	cout<<endl<<"Enter number 1=> ";
+	cin>>a;
+	cout<<endl<<"Enter number 2=> ";
+	cin>>b;
+	cout<<endl<<"Add = "<<a+b;
+	cout<<endl<<"Sub = "<<a-b;
+	cout<<endl<<"Divide = "<<a/b;
+	cout<<endl<<"Multiply = "<<a*b;
+}
