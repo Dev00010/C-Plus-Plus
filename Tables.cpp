6384b4fc9ca13d57925a76e9904cfa5af2888c7f
Dev <joisardev@gmail.com>
2022-01-15 15:58:47 +0530

    Prints table of user input number

---
+++ Tables.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+main()
+{
+	int a,b,i;
+	cout<<endl<<"Enter Table to print=> ";
+	cin>>a;
+	for(i=1;i<=10;i++)
+	{
+		cout<<endl<<a<<" * "<<i<<" = "<<a*i;
+	}
+}
