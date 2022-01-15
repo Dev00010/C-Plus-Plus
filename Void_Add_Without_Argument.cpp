ffd197f78b1f33cb48ae84441245695ad43a8a61
Dev <joisardev@gmail.com>
2022-01-15 16:03:46 +0530

    Void Function for addition without arguments

---
+++ Void_Add_Without_Argument.cpp
@@ -0,0 +1,15 @@
+#include<iostream>
+using namespace std;
+void add()
+{
+	int a,b;
+	cout<<endl<<"Enter Number 1=> ";
+	cin>>a;
+	cout<<endl<<"Enter Number 2=> ";
+	cin>>b;
+    cout<<endl<<"Addition of 2 number is=> "<<a+b;
+}
+main()
+{
+	add();
+}
